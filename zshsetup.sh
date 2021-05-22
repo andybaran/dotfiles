4f690115ec02e4820daf99c13c865e7717e8c3af
Andy Baran <andy.baran@hashicorp.com>
2021-05-22 14:51:31 -0400

    p10k and misc iterm2 changes

--- zshsetup.sh
+++ zshsetup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Lots of explanation here https://gist.github.com/kevin-smets/8568070
+# Lots of explanation here...but maybe ignore it? https://gist.github.com/kevin-smets/8568070
 
 cd /tmp
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -7,4 +7,4 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo ZSH_THEME="powerlevel10k/powerlevel10k" >> ~/.zshrc
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
 p10k configure
-wget https://github.com/Falkor/dotfiles/blob/master/fonts/SourceCodePro%2BPowerline%2BAwesome%2BRegular.ttf
+#wget https://github.com/Falkor/dotfiles/blob/master/fonts/SourceCodePro%2BPowerline%2BAwesome%2BRegular.ttf
